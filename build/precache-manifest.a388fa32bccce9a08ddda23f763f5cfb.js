1eff4c4969a35ab7c0ecf0e96fb11ac9b37002d2
garethjscoleman <garethjscoleman@rockpapersquid.com>
2019-01-04 20:46:02 +0000

    fix_homepage

---
+++ build/precache-manifest.a388fa32bccce9a08ddda23f763f5cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/breadandpizza/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d21e8b80e032d5a49c1f",
+    "url": "/breadandpizza/build/static/js/runtime~main.d21e8b80.js"
+  },
+  {
+    "revision": "a22677b64cb84c5050ed",
+    "url": "/breadandpizza/build/static/js/main.a22677b6.chunk.js"
+  },
+  {
+    "revision": "48924a4a06b41f460b42",
+    "url": "/breadandpizza/build/static/js/1.48924a4a.chunk.js"
+  },
+  {
+    "revision": "a22677b64cb84c5050ed",
+    "url": "/breadandpizza/build/static/css/main.9b5ec982.chunk.css"
+  },
+  {
+    "revision": "589023059997436d486176c628cb2f24",
+    "url": "/breadandpizza/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/breadandpizza/precache-manifest.74aaa8acf3245fa3ab4f354472c2d8be.js"
+  "/breadandpizza/build/precache-manifest.a388fa32bccce9a08ddda23f763f5cfb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/breadandpizza/index.html", {
+workbox.routing.registerNavigationRoute("/breadandpizza/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
